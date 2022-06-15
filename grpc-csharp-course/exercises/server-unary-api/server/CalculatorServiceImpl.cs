fbf6a842e2574d9e3b91a9d38f541ebf2e6f6fc7
Diego <diego.canizales1995@gmail.com>
2022-06-14 19:04:20 -0500

    Adding service impl.

---
+++ grpc-csharp-course/exercises/server-unary-api/server/CalculatorServiceImpl.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Calculator;
+using static Calculator.CalculatorService;
+using Grpc.Core;
+
+namespace server
+{
+    public class CalculatorServiceImpl : CalculatorServiceBase
+    {
+        public override Task<SumResponse> Sum(SumRequest request, ServerCallContext context)
+        {
+            int result = request.A + request.B;
+
+            return Task.FromResult(new SumResponse() { Result = result });
+        }
+    }
+}
